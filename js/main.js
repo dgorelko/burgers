48b0ac9e302211761d362e25027097b566506e95
Dmitry Gorelko <dmitry.gorelko@gmail.com>
2017-11-12 18:54:54 +0300

    Bugfix markup

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
 let hamburger = document.querySelector(".nav-hamburger");
 let menu = document.querySelector(".greeting__menu");
-console.log(menu);
-console.log(menu.style.width)
+
 hamburger.addEventListener("click", function() {
     if(hamburger.classList.contains("nav-hamburger_active")) {
-      	hamburger.classList.remove("nav-hamburger_active");
+          hamburger.classList.remove("nav-hamburger_active");
+          menu.classList.remove("greeting__menu_active");
 
     }else{
-  	    hamburger.classList.add("nav-hamburger_active");
-    }
-    if (menu.style.display == '') {    
-        menu.style.display = 'block';
-    } else{
-        menu.style.display = '';
+          hamburger.classList.add("nav-hamburger_active");
+          menu.classList.add("greeting__menu_active");
     }
 });
